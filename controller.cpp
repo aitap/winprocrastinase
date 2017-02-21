d4df5e02fe32db1c6bc1ea9533683a4c33f99786
Ivan Krylov <krylov.r00t@gmail.com>
2017-02-21 19:45:20 +0300

    more testing; switch clock to GetTickCount; BUGFIX: actually kill timers when they fire (they should be one time)

--- controller.cpp
+++ controller.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #include <set>
 #include <stdexcept>
 #include <memory>
-#include <chrono>
 
 #include <windows.h>
 #include <shellapi.h>
@@ -12,24 +11,29 @@ using namespace std;
 #include "win_utils.hpp"
 
 // constants
-const static float work_per_play = 2; // how many times more time user should spend working to afford same amount of play time
+//const static float work_per_play = 2; // how many times more time user should spend working to afford same amount of play time
+const static float work_per_play = 1; // how many times more time user should spend working to afford same amount of play time
 //const static uint32_t alarm_timeout = 60*1000; // time between alarm and projected (credit=0), ms
-const static uint32_t alarm_timeout = 5*1000; // time between alarm and projected (credit=0), ms
+const static uint32_t alarm_timeout = 3*1000; // time between alarm and projected (credit=0), ms
 
 // horrible global variables
 //int64_t play_credit = 3600*1000; // remaining non-work time, ms; user is given 1 hour for free at startup
-int64_t play_credit = 30*1000; // remaining non-work time, ms; user is given 1 hour for free at startup
+int64_t play_credit = 10*1000; // remaining non-work time, ms; user is given 1 hour for free at startup
 bool good = true; // whether the user is good for now
 std::set<std::string> whitelist; // set of paths allowed to be running indefinitely
 UINT_PTR alarm_timer = 0; // timer to alarm sound
 UINT_PTR kill_timer = 0; // timer to kill of the offender
 
-extern "C" void CALLBACK kill_callback(HWND unused, UINT message, UINT_PTR timer, DWORD now) {
+extern "C" void CALLBACK kill_callback(HWND hwnd, UINT message, UINT_PTR timer, DWORD now) {
+	KillTimer(hwnd, timer);
+	kill_timer = 0;
 	/*DEBUG*/ cout << "kill callback" << std::endl;
 	kill_window_process(GetForegroundWindow());
 }
 
-extern "C" void CALLBACK alarm_callback(HWND unused, UINT message, UINT_PTR timer, DWORD now) {
+extern "C" void CALLBACK alarm_callback(HWND hwnd, UINT message, UINT_PTR timer, DWORD now) {
+	KillTimer(hwnd, timer);
+	alarm_timer = 0;
 	/*DEBUG*/ cout << "alarm callback" << std::endl;
 	PlaySound("threat", GetModuleHandle(NULL), SND_RESOURCE|SND_ASYNC);
 	kill_timer = SetTimer(NULL, kill_timer, alarm_timeout, kill_callback);
@@ -37,17 +41,16 @@ extern "C" void CALLBACK alarm_callback(HWND unused, UINT message, UINT_PTR time
 
 extern "C" void CALLBACK foreground_changed
 (HWINEVENTHOOK ev_hook, DWORD event, HWND new_foreground, LONG object, LONG child, DWORD thread, DWORD time) {
-	static auto prev_event = std::chrono::steady_clock::now(); // remember time of previous event
-	// now - previous
-	auto time_diff_ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - prev_event).count();
-	prev_event = std::chrono::steady_clock::now(); // and remember now for the next time
+	static auto prev_event = GetTickCount(); // remember time of previous event
+	auto time_diff_ms = GetTickCount() - prev_event; // adjust credit using (now-previous)
+	prev_event = GetTickCount(); // and remember what time is it now for the next time
 
 	/*DEBUG*/cout << "window callback: " << time_diff_ms << " ms elapsed; credit: " << play_credit << " -> ";
 
-	// account for whatever was happening until switch
+	// account for whatever was happening until window switch
 	play_credit += (good ? 1/work_per_play : -1)*time_diff_ms;
-	// if something has been killed due to not switching to work, credit *is* overdrawn; we require positive values
-	if (play_credit < 0) play_credit = 0;
+	// with overdrawn credit, set timeout to minimal possible
+	if (play_credit < USER_TIMER_MINIMUM) play_credit = USER_TIMER_MINIMUM;
 
 	/*DEBUG*/cout << play_credit << std::endl;
 
