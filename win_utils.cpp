6a52fd2666b57adc1147de6de45e3d0755c8a66e
aitap <krylov.r00t@gmail.com>
2018-01-03 21:31:54 +0300

    upgrade win_utils to my own RAII wrapper around WIN32 types

--- win_utils.cpp
+++ win_utils.cpp
@@ -7,17 +7,17 @@ std::string get_window_process_path(HWND window) {
 	char path[2048];
 
 	DWORD thread_id = GetWindowThreadProcessId(window,NULL);
-	u_handle thread{OpenThread(THREAD_QUERY_LIMITED_INFORMATION, FALSE, thread_id),&CloseHandle};
+	handle thread = OpenThread(THREAD_QUERY_LIMITED_INFORMATION, FALSE, thread_id);
 	if (!thread) throw runtime_error(std::string("OpenThread returned ")+std::to_string(GetLastError()));
 
-	DWORD pid = GetProcessIdOfThread(thread.get());
+	DWORD pid = GetProcessIdOfThread(thread);
 	if (!pid) throw runtime_error(std::string("GetProcessIdOfThread returned ")+std::to_string(GetLastError()));
 
-	u_handle process{OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid),&CloseHandle};
+	handle process = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
 	if (!process) throw runtime_error(std::string("OpenProcess returned ")+std::to_string(GetLastError()));
 
 	DWORD size = 2048;
-	if (!QueryFullProcessImageName(process.get(), 0/*win32 format*/, path, &size))
+	if (!QueryFullProcessImageName(process, 0/*win32 format*/, path, &size))
 		throw runtime_error(std::string("QueryFullProcessImageName returned ")+std::to_string(GetLastError()));
 
 	return std::string(path);
@@ -37,21 +37,21 @@ std::string get_window_title(HWND window) {
 void kill_window_process(HWND window) {
 	using std::runtime_error;
 	DWORD thread_id = GetWindowThreadProcessId(window,NULL);
-	u_handle thread{OpenThread(THREAD_QUERY_LIMITED_INFORMATION, FALSE, thread_id),&CloseHandle};
+	handle thread = OpenThread(THREAD_QUERY_LIMITED_INFORMATION, FALSE, thread_id);
 	if (!thread) throw runtime_error(std::string("OpenThread returned ")+std::to_string(GetLastError()));
 
-	DWORD pid = GetProcessIdOfThread(thread.get());
+	DWORD pid = GetProcessIdOfThread(thread);
 	if (!pid) throw runtime_error(std::string("GetProcessIdOfThread returned ")+std::to_string(GetLastError()));
 
-	u_handle process{OpenProcess(PROCESS_TERMINATE, FALSE, pid),&CloseHandle};
+	handle process = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
 	if (!process) throw runtime_error(std::string("OpenProcess returned ")+std::to_string(GetLastError()));
 
-	if (!TerminateProcess(process.get(), 1)) throw runtime_error(std::string("TerminateProcess returned ")+std::to_string(GetLastError()));
+	if (!TerminateProcess(process, 1)) throw runtime_error(std::string("TerminateProcess returned ")+std::to_string(GetLastError()));
 }
 
 BY_HANDLE_FILE_INFORMATION get_file_info(const char* path) {
-	handle fh = (
-		CreateFile(path, 0, FILE_SHARE_DELETE|FILE_SHARE_READ|FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr)
+	handle fh = CreateFile(
+		path, 0, FILE_SHARE_DELETE|FILE_SHARE_READ|FILE_SHARE_WRITE, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr
 	);
 	if (fh == INVALID_HANDLE_VALUE) throw std::runtime_error(std::string("CreateFile returned ")+std::to_string(GetLastError()));
 	BY_HANDLE_FILE_INFORMATION ret;
